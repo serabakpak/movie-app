e22f4f0ea7b91a8a5ee7c5a1d8f01ddb0437c4aa
Sera Bak <serabak15@gmail.com>
2016-10-25 00:30:27 -0700

    testing to see if prefix fixes routing template issue on gh pages

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,7 @@ config.$inject = ['$routeProvider', '$locationProvider'];
 function config($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: '/templates/movies.html',
+			templateUrl: '#/templates/movies.html',
 			controller: 'IndexController',
 			controllerAs: 'indexCtrl'
 		})
