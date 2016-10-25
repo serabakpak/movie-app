edfa22b5a90dac77e7556701dba1b93760190c0f
Sera Bak <serabak15@gmail.com>
2016-10-25 00:44:31 -0700

    fixing templateUrls

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,12 +6,12 @@ config.$inject = ['$routeProvider', '$locationProvider'];
 function config($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: '/templates/movies.html',
+			templateUrl: 'templates/movies.html',
 			controller: 'IndexController',
 			controllerAs: 'indexCtrl'
 		})
 		.when('/:id', {
-			templateUrl: '/templates/show.html',
+			templateUrl: 'templates/show.html',
 			controller: 'ShowController',
 			controllerAs: 'showCtrl'
 		}).otherwise({
